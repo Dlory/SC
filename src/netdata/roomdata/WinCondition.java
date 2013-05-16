0e187ef7b92c775e4cad05f5bc6d8f9b59502846
wanghui <huahuai009@163.com>
2013-05-16 18:00:43 +0800

    属性值做了修改

--- src/netdata/roomdata/WinCondition.java
+++ src/netdata/roomdata/WinCondition.java
@@ -2,6 +2,6 @@ package netdata.roomdata;
 
 public class WinCondition {
 
-	public static final int TIME = 1;
-	public static final int SCORE = 2;
+	public static final int TIME = 2;
+	public static final int SCORE = 1;
 }
