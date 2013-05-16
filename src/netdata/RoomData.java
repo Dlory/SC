a3f483c65205f14b3c5a06e98f49eff1d6be0337
wanghui <huahuai009@163.com>
2013-05-16 15:33:35 +0800

    添加了raceType、subRaceType、reLiveTime等属性

--- src/netdata/RoomData.java
+++ src/netdata/RoomData.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 public class RoomData implements Serializable {
 	public int roomId = 0;
-	public int sceneId = 0;
-	public int winScore = 0;
-	public int rule = 0;
-	public int winCondition = 0;
+	public int sceneId = 0;//地图ID
+	public int winCondition = 0;//比赛规则，回合数、时间、杀人数等
+	public int winScore = 0;//比赛规则数值
+	public int raceType = 0;//比赛类型，团战、爆破战、特殊战等
+	public int subRaceType = 0;//子模式，刀战、手枪战、雷战等;
 	public int sectionTime = 0;
+	public int reLiveTime = 3;//复活时间(秒数)
 }
