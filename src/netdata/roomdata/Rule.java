ca6e74097ae5120551fe6da9dc95fd46c3ee4edd
jpy <jpy@wooduan.com>
2013-06-07 20:08:52 +0800

    增加字段特殊战,个人战

--- src/netdata/roomdata/Rule.java
+++ src/netdata/roomdata/Rule.java
@@ -1,6 +1,8 @@
 package netdata.roomdata;
 
 public class Rule {
+	public static final int EXPLODE = 2;	
 	public static final int GROUP = 3;
-	public static final int EXPLODE = 2;
+	public static final int SPECIAL_GROUP = 4;
+	public static final int PERSONAL = 5;
 }
---
+++ src/netdata/roomdata/SpecialsubRaceType.java
@@ -0,0 +1,8 @@
+package netdata.roomdata;
+
+public class SpecialsubRaceType {
+	public static final int WEAPONTYPE_KNIFE = 41;
+	public static final int WEAPONTYPE_SNIPER_RIFLE = 42;
+	public static final int WEAPONTYPE_PISTOL = 43;
+	public static final int WEAPONTYPE_BOMB = 44;
+}
