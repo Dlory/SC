a82530c3600891747ed76973abe057e78a55bf84
jpy <jpy@wooduan.com>
2014-10-23 11:29:28 +0800

    增加娱乐团队32

--- src/netdata/roomdata/Rule.java
+++ src/netdata/roomdata/Rule.java
@@ -3,6 +3,10 @@ package netdata.roomdata;
 public class Rule {
 	public static final int EXPLODE = 2;	
 	public static final int GROUP = 3;
+	/**
+	 * 娱乐团队 有大杀器
+	 */
+	public static final int GROUP_RECHARGE = 32;
 	public static final int SPECIAL_GROUP = 4;
 	public static final int PERSONAL = 5;
 	public static final int BIOCHEMICAL = 6;
@@ -10,5 +14,8 @@ public class Rule {
 	public static final int RECREATION_EXPLODE = 8;
 	public static final int KING_OF_GUN = 9;
 	public static final int GROUP_KING_OF_GUN = 10;
+	/**
+	 * 团队争霸
+	 */
 	public static final int RECREATION_GROUP = 11;
 }
