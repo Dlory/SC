8b9a45147dcc1a0c5d9b0fd00e4465e127cb8f99
Long Heng <lh@wooduan.com>
2015-04-22 13:25:29 +0800

    基础经验和GP计算代码移动要shareCommon

---
+++ src/battle/ConstantRace.java
@@ -0,0 +1,95 @@
+/*
+ * (C) 2012-2013 Wooduan Group.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ * Authors:
+ * duty <huahuai009@163.com>
+ */
+package battle;
+
+/**
+ * 模式常量
+ * @author duty <huahuai009@163.com>
+ */
+public class ConstantRace {	
+	/**
+	 * 爆破模式ID
+	 */
+	public static final int RACE_BAOPO_ID = 2;
+	/**
+	 * 团队模式ID
+	 */
+	public static final int RACE_TUANDUI_ID = 3;
+	/**
+	 * 特殊模式ID
+	 */
+	public static final int RACE_TESHU_ID = 4;
+	/**
+	 * 个人模式ID
+	 */
+	public static final int RACE_GEREN_ID = 5;
+	/**
+	 * 变异战ID
+	 */
+	public static final int RACE_BIANYI_ID = 6;
+	/**
+	 * 英雄对决ID
+	 */
+	public static final int RACE_YINGXIONG_ID = 7;
+	/**
+	 * 无限爆破ID
+	 */
+	public static final int RACE_WUXIAN_BAOPO_ID = 8;
+	/**
+	 * 枪王之王ID
+	 */
+	public static final int RACE_QIANGWANG_ID = 9;
+	/**
+	 * 团队枪王之王ID
+	 */
+	public static final int RACE_TUANDUI_QIANGWANG_ID = 10;
+	/**
+	 * 团队争霸ID
+	 */
+	public static final int RACE_TUANDUI_ZHENGBA_ID = 11;
+	/**
+	 * 娱乐团队模式ID
+	 */
+	public static final int RACE_YULE_TUANDUI_ID = 32;
+	/**
+	 * 死亡竞赛ID
+	 */
+	public static final int RACE_SIWANG_JINGSAI_ID = 12;
+	
+	/**
+	 * 生死爆破
+	 */
+	public static final int RACE_SHENGSI_BAOPO_ID = 13;
+	
+	/**
+	 * 终极变异战ID
+	 */
+	public static final int RACE_ZHONGJI_BIANYI_ID = 14;
+	
+	/**
+	 * 刀锋战士ID
+	 */
+	public static final int RACE_DAOFENG_ZHANSHI_ID = 15;
+	
+	/**
+	 * 超能变异ID
+	 */
+	public static final int RACE_CHAONENG_BIANYI_ID = 16;
+	
+	/**
+	 * 末日变异ID
+	 */
+	public static final int RACE_MORI_BIANYI_ID = 17;
+	
+	/**
+	 * 赏金爆破ID
+	 */
+	public static final int RACE_SHANGJIN_BAOPO_ID = 18;
+}
---
+++ src/battle/ExpAndGpCaculator.java
@@ -0,0 +1,259 @@
+package battle;
+
+import netdata.BattleReport;
+import netdata.UserReport;
+
+public class ExpAndGpCaculator
+{
+	public static int getBaseExp(BattleReport br, UserReport ur)
+	{
+		double baseExp = ur.duration / 60 * 20;
+		
+		double bf = 0;
+		if (br.totalNum <= 2) 
+		{
+			bf = 0.4;
+		} 
+		else if (br.totalNum <= 5) 
+		{
+			bf = 0.8;
+		} 
+		else if (br.totalNum <= 8) 
+		{
+			bf = 1;
+		} 
+		else if (br.totalNum <= 12) 
+		{
+			bf = 1.4;
+		} 
+		else if (br.totalNum <= 16) 
+		{
+			bf = 1.6;
+		} 
+		else 
+		{
+			bf = 1.8;
+		}
+		
+		double mf = 0;
+		switch (br.roomData.raceType) 
+		{
+			case ConstantRace.RACE_BAOPO_ID:
+				mf = 1;
+				break;
+			case ConstantRace.RACE_TUANDUI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_TESHU_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_GEREN_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_BIANYI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_YINGXIONG_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_WUXIAN_BAOPO_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_QIANGWANG_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_TUANDUI_QIANGWANG_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_YULE_TUANDUI_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_TUANDUI_ZHENGBA_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_SIWANG_JINGSAI_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_SHENGSI_BAOPO_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_DAOFENG_ZHANSHI_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_SHANGJIN_BAOPO_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_ZHONGJI_BIANYI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_CHAONENG_BIANYI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_MORI_BIANYI_ID:
+				mf = 0.9;
+				break;
+			default:
+				mf = 0.75;
+				break;
+		}
+		
+		if(br.roomData.raceType == ConstantRace.RACE_BIANYI_ID || br.roomData.raceType == ConstantRace.RACE_YINGXIONG_ID || br.roomData.raceType == ConstantRace.RACE_ZHONGJI_BIANYI_ID || br.roomData.raceType == ConstantRace.RACE_CHAONENG_BIANYI_ID || br.roomData.raceType == ConstantRace.RACE_MORI_BIANYI_ID) 
+		{
+			baseExp = (baseExp + Math.min(2000, ur.score) / 3.5 ) * bf * mf;
+		}
+		else
+		{
+			double kdf = 0;
+			double kd = 0;
+			if (ur.killNum > 0) {
+				kd = (double) ur.killNum / ur.beKillNum;
+			}
+			if (ur.beKillNum == 0) {
+				kd = (double) ur.killNum;
+			}
+
+			if (kd < 1) {
+				kdf = 0.9;
+			} else if (kd < 1.4) {
+				kdf = 1;
+			} else if (kd < 1.8) {
+				kdf = 1.1;
+			} else if (kd < 2) {
+				kdf = 1.15;
+			} else {
+				kdf = 1.2;
+			}
+			
+			baseExp += Math.min(100.0, (ur.headKill * 1.5 + (ur.killNum - ur.headKill) * 1));
+			baseExp = baseExp * bf * kdf * mf;
+		}
+		
+		return (int)Math.round(baseExp);
+	}
+
+	public static int getBaseGp(BattleReport br, UserReport ur) 
+	{
+		double baseGp = ur.duration / 60 * 18;
+		
+		double bf = 0;
+		if (br.totalNum <= 2) 
+		{
+			bf = 0.4;
+		} 
+		else if (br.totalNum <= 5) 
+		{
+			bf = 0.8;
+		} 
+		else if (br.totalNum <= 8) 
+		{
+			bf = 1;
+		} 
+		else if (br.totalNum <= 12) 
+		{
+			bf = 1.4;
+		} 
+		else if (br.totalNum <= 16) 
+		{
+			bf = 1.6;
+		} 
+		else 
+		{
+			bf = 1.8;
+		}
+		
+		double mf = 0;
+		switch (br.roomData.raceType) 
+		{
+			case ConstantRace.RACE_BAOPO_ID:
+				mf = 1;
+				break;
+			case ConstantRace.RACE_TUANDUI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_TESHU_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_GEREN_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_BIANYI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_YINGXIONG_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_WUXIAN_BAOPO_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_QIANGWANG_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_TUANDUI_QIANGWANG_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_YULE_TUANDUI_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_TUANDUI_ZHENGBA_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_SIWANG_JINGSAI_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_SHENGSI_BAOPO_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_DAOFENG_ZHANSHI_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_SHANGJIN_BAOPO_ID:
+				mf = 0.9;
+				break;
+			case ConstantRace.RACE_ZHONGJI_BIANYI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_CHAONENG_BIANYI_ID:
+				mf = 0.75;
+				break;
+			case ConstantRace.RACE_MORI_BIANYI_ID:
+				mf = 0.9;
+				break;
+			default:
+				mf = 0.75;
+				break;
+		}
+		
+		if(br.roomData.raceType == ConstantRace.RACE_BIANYI_ID || br.roomData.raceType == ConstantRace.RACE_YINGXIONG_ID || br.roomData.raceType == ConstantRace.RACE_ZHONGJI_BIANYI_ID || br.roomData.raceType == ConstantRace.RACE_CHAONENG_BIANYI_ID || br.roomData.raceType == ConstantRace.RACE_MORI_BIANYI_ID) 
+		{
+			baseGp = (baseGp + Math.min(2000, ur.score) / 3.5 ) * bf * mf;
+		}
+		else
+		{
+			double kdf = 0;
+			double kd = 0;
+			if (ur.killNum > 0) {
+				kd = (double) ur.killNum / ur.beKillNum;
+			}
+			if (ur.beKillNum == 0) {
+				kd = (double) ur.killNum;
+			}
+
+			if (kd < 1) {
+				kdf = 0.9;
+			} else if (kd < 1.4) {
+				kdf = 1;
+			} else if (kd < 1.8) {
+				kdf = 1.1;
+			} else if (kd < 2) {
+				kdf = 1.15;
+			} else {
+				kdf = 1.2;
+			}
+			
+			baseGp += Math.min(100, (int)(ur.headKill * 1.5 + (ur.killNum - ur.headKill) * 1));
+			baseGp = baseGp * bf * kdf * mf;
+		}
+		
+		return (int)Math.round(baseGp);
+	}
+}
