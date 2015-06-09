6f6285bc6ed2ffb8a32629bd57354d8051a11c71
jpy <jpy@wooduan.com>
2015-06-09 10:03:28 +0800

    战报增加治疗量

--- src/main/java/netdata/UserReport.java
+++ src/main/java/netdata/UserReport.java
@@ -80,6 +80,7 @@ public class UserReport implements Serializable {
 	public int changeHeroNum = 0;//变身英雄次数
 	public boolean[] sectionBestKill = null;//枪王赛各阶段最佳标志
 	public boolean isNurse = false;//战地医生
+	public int treatment = 0;//治疗量
 	
 	public int battleAddExp = 0; //战斗荣耀经验加成
 	public int battleAddGp = 0;//战斗荣耀gp加成
